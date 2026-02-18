d3b2fde872e2ea445e04422c385559876b590f74
Jean <jeamcrv@hotmail.com>
2026-02-18 15:27:08 -0300

    Add Desktop component with wallpaper background and grid layout

---
+++ src/components/layout/desktop.tsx
@@ -0,0 +1,19 @@
+import WallpaperImage from "@/assets/wallpaper.jpg";
+
+export function Desktop() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${WallpaperImage.src})`,
+        gridTemplateRows: "[taskbar] auto [desktop] 1fr [dock] auto",
+      }}
+      className="relative grid h-screen bg-cover bg-top select-none"
+    >
+      <div style={{ gridRow: "taskbar" }}>Taskbar</div>
+
+      <div style={{ gridRow: "desktop" }}>Desktop</div>
+
+      <div style={{ gridRow: "dock" }}>Dock</div>
+    </div>
+  );
+}
