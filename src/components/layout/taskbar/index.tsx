809792e45259eb06dc69b59846ac253ac91daba8
Jean <jeamcrv@hotmail.com>
2026-02-23 13:33:07 -0300

    Add Weather component to Taskbar

--- src/components/layout/taskbar/index.tsx
+++ src/components/layout/taskbar/index.tsx
@@ -1,10 +1,15 @@
 import { StartDropdown } from "./start-dropdown";
+import { Weather } from "./weather";
 
 export function Taskbar() {
   return (
     <div className="bg-background/50 text-foreground flex items-center justify-between px-2">
       <div className="flex gap-8">
         <StartDropdown />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Weather />
       </div>
     </div>
   );
