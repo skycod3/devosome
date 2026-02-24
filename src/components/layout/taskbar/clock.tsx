f87a68469fdc67660146e166df9113f434960fff
Jean <jeamcrv@hotmail.com>
2026-02-24 09:58:15 -0300

    Add Clock component to display current time

---
+++ src/components/layout/taskbar/clock.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+
+export function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    // calculate milliseconds until next minute
+    const now = new Date();
+    const msUntilNextMinute =
+      (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+
+    // sync to start of next minute
+    const syncTimeout = setTimeout(() => {
+      setTime(new Date());
+
+      // then update every minute
+      interval = setInterval(() => setTime(new Date()), 60 * 1000);
+    }, msUntilNextMinute);
+
+    // cleanup both timeout and interval
+    return () => {
+      clearTimeout(syncTimeout);
+      if (interval) clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <p>
+      {format(time, "EEE")} {format(time, "HH:mm")}
+    </p>
+  );
+}
