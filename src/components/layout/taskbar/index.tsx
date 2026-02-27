6cb610e12fb59b3cf15bb684a7a71f680674f62f
Jean <jeamcrv@hotmail.com>
2026-02-27 14:49:31 -0300

    Add WindowsDropdown to Taskbar for managing open windows

--- src/components/layout/taskbar/index.tsx
+++ src/components/layout/taskbar/index.tsx
@@ -2,12 +2,19 @@ import { StartDropdown } from "./start-dropdown";
 import { Weather } from "./weather";
 import { Clock } from "./clock";
 import { SystemTray } from "../system-tray";
+import { WindowsDropdown } from "./windows.dropdown";
+
+import { useWindows } from "@/hooks/useWindows";
 
 export function Taskbar() {
+  const { windows } = useWindows();
+
   return (
     <div className="bg-background/50 text-foreground flex items-center justify-between px-2">
       <div className="flex gap-8">
         <StartDropdown />
+
+        {windows.length > 0 && <WindowsDropdown />}
       </div>
 
       <div className="flex items-center gap-4">
