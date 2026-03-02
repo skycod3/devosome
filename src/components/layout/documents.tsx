06ffcc616836b4fe0ed169797c6650eb62e993c8
Jean <jeamcrv@hotmail.com>
2026-03-02 13:12:12 -0300

    Add Documents component to display resume icon with dynamic iconId

---
+++ src/components/layout/documents.tsx
@@ -0,0 +1,42 @@
+import { useIcons } from "@/hooks/useIcons";
+import { useEffect, useMemo } from "react";
+
+import ResumeImage from "@/assets/resume.svg";
+
+import { Icon } from "../icon";
+
+interface DocumentsProps {
+  iconId: string;
+}
+
+export function Documents({ iconId }: DocumentsProps) {
+  const { addIcon, removeIcon } = useIcons();
+
+  const resumeIcon = useMemo(
+    () => ({
+      id: "icon-resume",
+      title: "Resume",
+      isHighlighted: false,
+      show: true,
+      icon: ResumeImage,
+      size: { width: 48, height: 48 },
+      parentId: iconId,
+    }),
+    [iconId],
+  );
+
+  useEffect(() => {
+    addIcon(resumeIcon);
+
+    return () => {
+      removeIcon(resumeIcon.id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="grid-cols-fill-6 grid h-full items-start gap-4">
+      <Icon {...resumeIcon} />
+    </div>
+  );
+}
