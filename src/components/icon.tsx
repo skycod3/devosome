823e797cb7b6625e3cf0f38524cf09ef109659ba
Jean <jeamcrv@hotmail.com>
2026-03-03 13:10:09 -0300

    Enhance Icon component to support dynamic icon color based on parentId and theme

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -6,12 +6,21 @@ import { CSSProperties } from "react";
 
 import { useIcons } from "@/hooks/useIcons";
 import { useWindows } from "@/hooks/useWindows";
+import { useTheme } from "@/hooks/useTheme";
 
 interface IconProps extends IconFromStore {}
 
-export function Icon({ id, title, icon, size, isHighlighted }: IconProps) {
+export function Icon({
+  id,
+  title,
+  icon,
+  size,
+  isHighlighted,
+  parentId,
+}: IconProps) {
   const { highlightIcon, unhighlightAllIcons } = useIcons();
   const { openWindowCentered } = useWindows();
+  const { theme } = useTheme();
 
   function handleClick() {
     // Only unhighlight all if this icon isn't already highlighted
@@ -32,7 +41,12 @@ export function Icon({ id, title, icon, size, isHighlighted }: IconProps) {
       title={title}
       style={
         {
-          "--icon-color": "white",
+          // if the icon has a parent, it should be black in light mode and white in dark mode, otherwise it should always be white
+          "--icon-color": parentId
+            ? theme === "dark"
+              ? "white"
+              : "black"
+            : "white",
         } as CSSProperties
       }
       className={`grid min-h-28 w-full content-center justify-items-center gap-2 rounded p-1 text-center text-(--icon-color) hover:bg-(--icon-color)/10 ${isHighlighted ? "bg-(--icon-color)/20" : ""}`}
