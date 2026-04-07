d33102cbcd9d48c85bc80e82e9a4470648cb2864
Jean <jeamcrv@hotmail.com>
2026-04-07 13:21:17 -0300

    feat(images): add css-is-awesome image and corresponding image file interface

---
+++ src/constants/image-files.ts
@@ -0,0 +1,18 @@
+import type { StaticImageData } from "next/image";
+import CssMemeImage from "@/assets/css-is-awesome.jpg";
+
+export interface ImageFile {
+  id: string;
+  title: string;
+  icon: StaticImageData;
+  windowTitle: string;
+}
+
+export const IMAGE_FILES: Record<string, ImageFile> = {
+  "image-css-is-awesome": {
+    id: "image-css-is-awesome",
+    title: "css-is-awesome.jpg",
+    icon: CssMemeImage,
+    windowTitle: "css-is-awesome.jpg",
+  },
+};
