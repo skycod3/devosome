3c5f68ac3425fdf2eb0eda7ba5fdb2d82d30bce1
Jean <jeamcrv@hotmail.com>
2026-04-09 07:54:08 -0300

    feat(icons): add Skills icon to desktop icons list

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,6 +1,7 @@
 import { Icon } from "@/stores/icons-store";
 
 import DocumentsImage from "@/assets/documents.svg";
+import SkillsImage from "@/assets/skills.svg";
 
 export const DESKTOP_ICONS: Icon[] = [
   {
@@ -10,5 +11,13 @@ export const DESKTOP_ICONS: Icon[] = [
     isHighlighted: false,
     show: true,
     size: { width: 48, height: 48 },
+  },
+  {
+    id: "icon-skills",
+    title: "Skills",
+    icon: SkillsImage,
+    isHighlighted: false,
+    show: true,
+    size: { width: 54, height: 54 },
   },
 ];
