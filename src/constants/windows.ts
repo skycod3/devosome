fb3e88b959de78b610da0ca6e212bc808272af40
Jean <jeamcrv@hotmail.com>
2026-04-29 14:13:36 -0300

    feat(constants): add TABLET_WINDOW_SIZE for improved responsive design

--- src/constants/windows.ts
+++ src/constants/windows.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_WINDOW_SIZE = { width: 800, height: 600 };
+export const TABLET_WINDOW_SIZE = { width: 600, height: 480 };
 export const DEFAULT_WINDOW_POSITION = { x: 100, y: 100 };
 export const BASE_Z_INDEX = 10;
