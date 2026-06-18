5d7ca0ee11e072c01f7d3336cfa380ae8de2d0c8
Jean <jeamcrv@hotmail.com>
2026-06-18 09:52:21 -0300

    feat: add grid distortion effect with CSS module

--- src/components/effects/grid-distortion.tsx
+++ src/components/effects/grid-distortion.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import "./grid-distortion.css";
+import styles from "./grid-distortion.module.css";
 
 const vertexShader = `
 uniform float time;
@@ -282,7 +282,7 @@ const GridDistortion = ({
   return (
     <div
       ref={containerRef}
-      className={`distortion-container ${className}`}
+      className={`${styles.distortionContainer} ${className}`}
       style={{
         width: "100%",
         height: "100%",
