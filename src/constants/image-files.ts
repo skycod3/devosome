d705143c2b072a035df7f4196fc7df675585b8e8
Jean <jeamcrv@hotmail.com>
2026-06-26 13:27:50 -0300

    feat: add new meme images and update image constants

--- src/constants/image-files.ts
+++ src/constants/image-files.ts
@@ -2,6 +2,8 @@ import type { StaticImageData } from "next/image";
 import type { FileDetails } from "@/types/files";
 import CssMemeImage from "@/assets/images/photos/css-is-awesome.jpg";
 import ThanksBrendan from "@/assets/images/photos/thanks-brendan.jpg";
+import MemeHtmlDev from "@/assets/images/photos/meme-html-dev.jpg";
+import MemeNodeModules from "@/assets/images/photos/meme-node-modules.png";
 
 export interface ImageFile {
   id: string;
@@ -39,6 +41,36 @@ export const IMAGE_FILES: Record<string, ImageFile> = {
       dimensions: {
         width: 639,
         height: 724,
+      },
+    },
+  },
+  "image-meme-html-dev": {
+    id: "image-meme-html-dev",
+    title: "html-dev.jpg",
+    icon: MemeHtmlDev,
+    windowTitle: "html-dev.jpg",
+    details: {
+      type: "JPG",
+      size: 1024 * 71.3,
+      createdAt: new Date("2026-06-25T13:17:00Z"),
+      dimensions: {
+        width: 500,
+        height: 616,
+      },
+    },
+  },
+  "image-meme-node-modules": {
+    id: "image-meme-node-modules",
+    title: "node-modules.png",
+    icon: MemeNodeModules,
+    windowTitle: "node-modules.png",
+    details: {
+      type: "PNG",
+      size: 1024 * 66.7,
+      createdAt: new Date("2026-06-25T13:17:00Z"),
+      dimensions: {
+        width: 990,
+        height: 712,
       },
     },
   },
