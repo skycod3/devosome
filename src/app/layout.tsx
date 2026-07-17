16a12cf27d2ba57c8758746ea50af2c89d51f191
Jean <jeamcrv@hotmail.com>
2026-07-17 10:04:09 -0300

    chore(layout): comment out ReactScanInit component in RootLayout for potential debugging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ReactScanInit } from "@/components/dev/react-scan-init";
+// import { ReactScanInit } from "@/components/dev/react-scan-init";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
 
@@ -76,7 +76,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${firaCode.variable} ${caveat.variable} antialiased`}>
-        <ReactScanInit />
+        {/* <ReactScanInit /> */}
         <ThemeProvider>
           <TooltipProvider>{children}</TooltipProvider>
 
